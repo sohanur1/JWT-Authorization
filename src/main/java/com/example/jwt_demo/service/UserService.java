9eb680a082f6f7a45e717acf0257f3516846e266
sohanur <shawon411184@gmail.com>
2021-10-18 14:22:40 +0600

    initial commit

---
+++ src/main/java/com/example/jwt_demo/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.jwt_demo.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+        //logic to get the user from database
+
+        return new User("admin","password", new ArrayList<>());
+    }
+}
